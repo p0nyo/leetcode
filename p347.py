3034e0d9c802e1843253e4f5f9d999fdc2a9e2d7
Treyson Tsen <treysontsen@treyson-mba3.local>
2025-01-03 16:09:33 +1300

    solved: problem 347

---
+++ p347.py
@@ -0,0 +1,27 @@
+def topKFrequent(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    dict = {} 
+    for num in nums:
+        if num in dict:
+            dict[num] += 1
+        else:
+            dict[num] = 0
+
+    sorted_dict = sorted(dict.items(), key=lambda x:x[1])
+    sorted_dict.reverse()
+
+    result = []
+
+    for i in range(k):
+        result.append(sorted_dict[i][0])
+
+    return result
+
+nums = [1,1,1,2,2,3]
+k = 2
+
+topKFrequent(nums, k)
