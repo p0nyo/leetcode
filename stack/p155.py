c0322d6c0c824860fbb7fe431905b66a55f45322
Treyson Tsen <tsentreyson@gmail.com>
2025-01-07 14:12:36 +1300

    solved: problem 155

--- stack/p155.py
+++ stack/p155.py
@@ -1,8 +1,7 @@
 class MinStack(object):
     def __init__(self):
         self.stack = []
-        self.min = None
-        self.dict = {}
+        self.min_stack = [None]
 
     def push(self, val):
         """
@@ -10,22 +9,21 @@ class MinStack(object):
         :rtype: None
         """
         self.stack.append(val)
-
-        if val in self.dict:
-            self.dict[val] += 1
-        else:
-            self.dict[val] = 1
-        if self.min == None:
-            self.min = val
+        if len(self.min_stack) != 0:
+            if self.min_stack[-1] == None:
+                self.min_stack.append(val)
+            elif val <= self.min_stack[-1]:
+                self.min_stack.append(val)
         
 
     def pop(self):
         """
         :rtype: None
         """
-        self.stack.pop()
-
-        
+        val = self.stack.pop()
+        if len(self.min_stack) != 0:
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self):
         """
@@ -33,29 +31,42 @@ class MinStack(object):
         """
         if len(self.stack) == 0:
             return None
-        return self.stack[len(self.stack)-1]
+        return self.stack[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.min
-    
-    def printDict(self):
-        print(self.dict)
+        if len(self.min_stack) == 0:
+            return None
+        return self.min_stack[-1]
         
 
 
 # Your MinStack object will be instantiated and called as such:
-val = 1
+
+
+# minStack = MinStack()
+# minStack.push(-2)
+# minStack.push(0)
+# minStack.push(-3)
+
+# print(minStack.stack)
+# print(minStack.min_stack)
+
+# print(minStack.getMin())
+# minStack.pop()
+# print(minStack.top())
+# print(minStack.getMin())
+
 
 obj = MinStack()
-obj.push(val)
-# obj.pop()
-param_3 = obj.top()
-param_4 = obj.getMin()
-
-print(param_3)
-
-obj.printDict()
+obj.push(0)
+obj.push(1)
+obj.push(0)
+print(obj.stack)
+print(obj.min_stack)
+print(obj.getMin())
+obj.pop()
+print(obj.getMin())
