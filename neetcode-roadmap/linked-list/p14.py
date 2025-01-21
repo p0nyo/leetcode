578c10bf2556fe9c46f643a35927514612d6d74c
Treyson Tsen <tsentreyson@gmail.com>
2025-01-21 22:51:23 +1300

    fix: solved in O(1) space complexity

--- neetcode-roadmap/linked-list/p14.py
+++ neetcode-roadmap/linked-list/p14.py
@@ -19,3 +19,27 @@ def removeNthFromEnd(head, n):
     first_node.next = first_node.next.next
 
     return head
+
+# Trying O(1) space complexity
+def removeNthFromEnd2(head, n):
+    """
+    :type head: Optional[ListNode]
+    :type n: int
+    :rtype: Optional[ListNode]
+    """
+    l = head
+    r = head
+    for _ in range(n+1):
+        r = r.next
+        if r.next == None:
+            return head.next
+
+    while r:
+        l = l.next
+        r = r.next
+
+    l.next = l.next.next
+
+    return head
+    
+    
