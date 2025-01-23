7cfaeff87eb7971891dc81e3f9500e7ddd78063f
Treyson Tsen <tsentreyson@gmail.com>
2025-01-23 22:07:47 +1300

    solved: problem 2

--- neetcode-roadmap/linked-list/p2.py
+++ neetcode-roadmap/linked-list/p2.py
@@ -14,22 +14,24 @@ def addTwoNumbers(l1, l2):
     current2 = l2
     res = ListNode(0)
 
-    def backtrack(head):
+    def backtrack(head, current1, current2, carry):
         if not current1 and not current2:
+            if carry > 0:
+                return ListNode(carry, None)
             return None
         
-        if current1 == None:
+        if not current1:
             val1 = 0
         else:
             val1 = current1.val
 
-        if current2 == None:
+        if not current2:
             val2 = 0
         else:
             val2 = current2.val
 
         val = val1 + val2 + carry
-        carry = val % 10
+        carry = val // 10
         val = val - (carry * 10)
 
         if current1:
@@ -38,11 +40,11 @@ def addTwoNumbers(l1, l2):
             current2 = current2.next
 
         new_node = ListNode(val)
-        new_node.next = backtrack(new_node)
+        new_node.next = backtrack(new_node, current1, current2, carry)
         
         return new_node
     
-    res.next = backtrack(res)
+    res.next = backtrack(res, current1, current2, carry)
 
     return res.next
         
