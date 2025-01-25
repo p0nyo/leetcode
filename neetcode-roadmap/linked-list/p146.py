aadbb3cbe119b4ee8de92f52cd799ed3ad56d058
Treyson Tsen <tsentreyson@gmail.com>
2025-01-25 14:36:15 +1300

    solved: problem 146 used py3 for collections module

---
+++ neetcode-roadmap/linked-list/p146.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
